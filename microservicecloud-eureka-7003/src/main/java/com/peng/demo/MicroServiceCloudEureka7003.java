3a8b2c15bb855edbc3d0ca2d62ed5d617ae6e9ab
pengkun <1473375907@qq.com>
2018-11-28 17:38:53 +0800

    springcloud 公司第二次提交

---
+++ microservicecloud-eureka-7003/src/main/java/com/peng/demo/MicroServiceCloudEureka7003.java
@@ -0,0 +1,13 @@
+package com.peng.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer//开启注册中心服务端
+@SpringBootApplication
+public class MicroServiceCloudEureka7003 {
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceCloudEureka7003.class, args);
+	}
+}
